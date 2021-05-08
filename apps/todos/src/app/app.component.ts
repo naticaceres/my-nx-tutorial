dab64ba214452155e4813518690c9dbe74694064
naticaceres <nataliacaceres91@gmail.com>
2021-05-08 12:19:19 -0300

    adding missing new files

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Todo } from '@my-nx-tutorial/data';
 @Component({
   selector: 'my-nx-tutorial-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
---
+++ libs/ui/src/lib/todos/todos.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '@my-nx-tutorial/data';
+
+@Component({
+  selector: 'my-nx-tutorial-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent {
+  @Input() todos: Todo[];
+}
